c57cb088c6077cbb9ac023d27a1c5eac93775d4f
Tanmay Jain <tanmay@Tanmays-MacBook-Air.local>
2024-12-28 20:28:28 +0530

    Hero Section Responsive complete

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,12 +1,13 @@
 export default function Hero() {
     return (
         <div>
-            <div className="mx-auto flex-col justify-center gap-4 w-full">
-                <div>
-                    <span className="text-5xl bg-gradient-to-b from-blue-400 to-blue-700 text-transparent bg-clip-text font-satoshiBlack">100xDevs,</span>
-                    <span className="text-5xl text-white/80  font-satoshiBlack">because 10x ain't enough!</span>
+            <div className="mx-auto flex flex-col items-center gap-8 w-full py-16">
+                <div className="text-center max-w-screen-md">
+                    <span className="text-5xl bg-gradient-to-b from-blue-400 to-blue-700 text-transparent bg-clip-text font-satoshiBlack md:text-7xl">100xDevs,</span>
+                    {" "}
+                    <span className="text-5xl bg-gradient-to-b from-white/90 to-white/60 text-transparent bg-clip-text font-satoshiBlack md:text-7xl">because 10x ain't enough!</span>
                 </div>
-                <div className="text-white/80">A beginner-friendly platform for mastering programming skills.</div>
+                <div className="text-white/80 text-center text-lg md:text-xl">A beginner-friendly platform for mastering programming skills.</div>
                 <div className="flex justify-normal items-center gap-4 text-sm">
                     <div className="rounded-lg text-white bg-gradient-to-b from-blue-400 to-blue-700 px-8 py-3">Explore Courses</div>
                     <div className="rounded-lg text-black bg-white px-8 py-3">Explore Notes</div>
